3c8d431f3f26ca631ee758c5570a18113456612b
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2022-04-07 01:02:28 -0300

    Fix follower

--- Playground/Assets/Scripts/Follower.cs
+++ Playground/Assets/Scripts/Follower.cs
@@ -6,9 +6,4 @@ public class Follower : MonoBehaviour
 {
     [SerializeField] public GameObject targetObject;
     [SerializeField] public float xOffset, yOffset, zOffset;
-
-    private void Update()
-    {
-        transform.Translate(V)
-    }
 }
